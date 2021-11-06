b35af7f3e1805e29e9a2eb8cf4c60806ed1eb5ae
surajit-ai <sdas49610@gmail.com>
2021-11-06 12:58:06 +0530

    changed

--- src/components/UserAfterLogin.js
+++ src/components/UserAfterLogin.js
@@ -1,8 +1,12 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Redirect } from "react-router-dom";
+import axios from 'axios';
+import { Col, Container, Row, Button, Card, Tabs, Tab, CardGroup, Carousel } from 'react-bootstrap';
+import Caro from "./Carousel";
 import Navbars from './Navbars';
+import Footer1 from './Footer1';
 
-function UserAfterLogin() {
+function UserAfterLogin(props) {
   // let authuser = sessionStorage.getItem('Key_Veriable')
   // console.log(authuser)
   // if (authuser == null) {
@@ -12,6 +16,43 @@ function UserAfterLogin() {
   // let name = sessionStorage.getItem('name');
   let name = sessionStorage.getItem('uid');
   // console.log(authuser)
+  const [newslist, setNewsList] = useState([]);
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    axios.get('http://localhost:4500/news/home')
+      .then(response => {
+        setNewsList(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+  }, [])
+  function viewNews() {
+    return newslist.map((currentrow, index) => {
+      return (
+        <Col key={index} style={{ width: "" }} >
+          <Card style={{ width: '', cursor: 'pointer', marginTop: '20px' }}>
+
+            <Card.Img variant="top" src={currentrow.nimg} style={{ maxHeight: "300px" }} />
+            <small className="text-muted"> {currentrow.ncat}</small>
+            <Card.Body>
+              <Card.Title style={{ maxHeight: "26px", marginTop: "5px", overflow: "hidden" }}>{currentrow.ntitle}.</Card.Title>
+              <Card.Text style={{ maxHeight: "26px", marginTop: "5px", overflow: "hidden" }}>{currentrow.ndesp}</Card.Text>
+              <Button variant="dark" size="sm" onClick={() => readmore(index)}>Read More</Button>
+            </Card.Body>
+          </Card>
+        </Col>
+
+      )
+    })
+  }
+
+  function readmore(index) {
+    var temp = [...newslist];
+    localStorage.setItem('readmore', temp[index]._id)
+    props.history.push('/AboutNews');
+  }
   if (name == null) {
     return (<Redirect to="/LoginPage" />)
   }
@@ -22,8 +63,20 @@ function UserAfterLogin() {
       <div>
         <Navbars />
         <br />
-       <center> <h3>WELCOME {name}</h3>
-        <h3>THIS IS USER DASH BOARD</h3></center>
+        <center><h3 className="f" style={{ marginLeft: 'px' }}>WELCOME {name}</h3></center>
+        <Caro />
+        <br />
+        <Container>
+          <Row xs={1} md={2} className="g-6" >
+            {viewNews()}
+          </Row>
+
+        </Container>
+        <hr class="new2"></hr>
+        &nbsp;
+        &nbsp;
+        &nbsp;
+        <Footer1 />
       </div>
     )
   }
