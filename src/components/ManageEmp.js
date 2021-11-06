ff758d4df0bdf3a04e2b350c5dfabbd175bab79a
surajit-ai <sdas49610@gmail.com>
2021-11-06 19:07:09 +0530

    manage change

--- src/components/ManageEmp.js
+++ src/components/ManageEmp.js
@@ -24,24 +24,59 @@ function ManageEmp() {
   }, [])
 
 
+
+
   function viewEmpList() {
     return emplist.map((currentrow, index) => {
-      console.log(index)
-      return (
-        <tr key={index}>
-          <td>{currentrow.empname}</td>
-          <td>{currentrow.empemail}</td>
-          <td>{currentrow.empmobile}</td>
-          {/* <td>{currentrow.empdob}</td>
-          <td>{currentrow.empgender}</td>
-          <td>{currentrow.empcountry}</td>
-          <td>{currentrow.empaddress}</td> */}
-          <td><button onClick={() => removeRow(index)} className="btn btn-danger">Delete</button></td>
-          {/* <td><button onClick={() => updateRow(index)} className="btn btn-primary">Update</button></td> */}
-        </tr>
-      )
-    })
+      if (currentrow.status === "0") {
+        return (
+          <tr key={index}>
+            {/* <td>{currentrow.nname}</td> */}
+
+            <td>{currentrow.empname}</td>
+            <td>{currentrow.empemail}</td>
+            <td>{currentrow.empmobile}</td>
+            <td><button onClick={() => approve(index)} className="btn btn-success" >Approve</button> </td>
+            <td><button onClick={() => removeRow(index)} className="btn btn-danger">Delete</button></td>
+          </tr>
+        );
+      }
+      else if (currentrow.status === "1") {
+        return (
+          <tr key={index}>
+            <td>{currentrow.empname}</td>
+            <td>{currentrow.empemail}</td>
+            <td>{currentrow.empmobile}</td>
+            {/* <td>{currentrow.empdob}</td>
+                            <td>{currentrow.empgender}</td>
+                            <td>{currentrow.empcountry}</td>
+                        <td>{currentrow.empaddress}</td> */}
+            {/* <td><button onClick={() => updateRow(index)} className="btn btn-primary">Update</button></td> */}
+            <td><button onClick={() => reject(index)} className="btn btn-danger" >Disapprove</button> </td>
+            <td><button onClick={() => removeRow(index)} className="btn btn-danger">Delete</button></td>
+          </tr>
+        );
+      }
+
+      else if (currentrow.status === "-1") {
+        return (
+          <tr key={index}>
+            <td>{currentrow.empname}</td>
+            <td>{currentrow.empemail}</td>
+            <td>{currentrow.empmobile}</td>
+            {/* <td>{currentrow.empdob}</td>
+                            <td>{currentrow.empgender}</td>
+                            <td>{currentrow.empcountry}</td>
+                        <td>{currentrow.empaddress}</td> */}
+            {/* <td><button onClick={() => updateRow(index)} className="btn btn-primary">Update</button></td> */}
+            <td><button onClick={() => approve(index)} className="btn btn-success" >Approve</button> </td>
+            <td><button onClick={() => removeRow(index)} className="btn btn-danger">Delete</button></td>
+          </tr>
+        );
+      }
+    });
   }
+
 
   function removeRow(index) {
     var tempemplist = [...emplist]; // make a new copy of array instead of mutating the same array directly. 
@@ -66,43 +101,72 @@ function ManageEmp() {
     setStatus(false)
     setEmpEmailId(removerow[0].empemail)
   }
+  function approve(index) {
+    var temp = [...emplist];
+    axios.put('https://gmitenewsbackend.herokuapp.com/emp//approve/' + temp[index]._id)
+      .then(response => {
+        console.log(response)
+        temp[index].status = "1"
+        setEmpList(temp);
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }
+  function reject(index) {
+    var temp = [...emplist];
+    axios.put('https://gmitenewsbackend.herokuapp.com/emp/reject/' + temp[index]._id)
+      .then(response => {
+        // console.log(response)
+        temp[index].status = "-1"
+        setEmpList(temp);
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }
 
-  if (status === true) {
-    return (
-      <div>
-        <Navbars />
-        <br />
-        <h3 align="center">USER DETAILS</h3>
-        <b style={{ color: "red" }}>{msg}</b>
-        <Container style={{ overflowX: 'auto' }}>
-          <Table striped bordered hover variant="dark">
-            <thead>
-              <tr>
-                <th>Name</th>
-                <th>Email</th>
-                <th>Mobile</th>
-                {/* <th>DOB</th>
+  return (
+    <div>
+      {/* <NavigationBar /> */}
+      <Navbars />
+      <br />
+      <h3 align="center">USER DETAILS</h3>
+      <b style={{ color: "red" }}>{msg}</b>
+      {/* <table border="1" align="center" striped bordered hover variant="dark">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Mobile</th> */}
+      {/* <th>DOB</th>
+            <th>Gender</th>
+            <th>Country</th>
+            <th>Address</th> */}
+      {/* </tr>
+        </thead> */}
+      <Container style={{ overflowX: 'auto' }}>
+        <Table striped bordered hover variant="dark">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Mobile</th>
+              {/* <th>DOB</th>
               <th>Gender</th>
               <th>Country</th>
               <th>Address</th> */}
-              </tr>
-            </thead>
+            </tr>
+          </thead>
 
-            <tbody>
-              {viewEmpList()}
-            </tbody>
-          </Table>
-        </Container>
-      </div>
-    )
-  }
-  else {
-    return (
-      <div>
-        <UpdateProfile email={empemailid} />
-      </div>
-    )
-  }
+          <tbody>
+            {viewEmpList()}
+          </tbody>
+        </Table>
+      </Container>
+    </div>
+  )
 }
+
 
 export default ManageEmp
