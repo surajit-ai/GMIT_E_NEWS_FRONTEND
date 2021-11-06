8381293add9b14778774fba1481021908cf899f1
surajit-ai <sdas49610@gmail.com>
2021-11-06 11:44:44 +0530

    cnd

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -6,48 +6,48 @@ import img5 from './img/caro2.jpeg'
 
 function Caro() {
   return (
-    <center>
-      <div>
-        <Carousel variant="dark" style={{ width: "94%" }}>
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src={img3}
-              alt="First slide"
-              style={{ maxHeight: "400px" }}
-            />
-            <Carousel.Caption>
-              <h5>Education</h5>
-              <p> CBSE Puts Out All-new Optical Mark Recognition Sheets For MCQ First-term Exams;</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src={img4}
-              alt="Second slide"
-              style={{ maxHeight: "400px" }}
-            />
-            <Carousel.Caption>
-              <h5>Gov.</h5>
-              <p>Venkaiah Naidu Affirms Women Empowerment 'essential For Accelerated National Progress'</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src={img5}
-              alt="Third slide"
-              style={{ maxHeight: "400px" }}
-            />
-            <Carousel.Caption>
-              <h5>Third slide label</h5>
-              <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-        </Carousel>
-      </div>
-    </center>
+
+    <center><div>
+      <Carousel variant="dark" style={{ width: "94%" }}>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={img3}
+            alt="First slide"
+            style={{ maxHeight: "400px" }}
+          />
+          <Carousel.Caption>
+            <h5>Education</h5>
+            <p> CBSE Puts Out All-new Optical Mark Recognition Sheets For MCQ First-term Exams;</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={img4}
+            alt="Second slide"
+            style={{ maxHeight: "400px" }}
+          />
+          <Carousel.Caption>
+            <h5>Gov.</h5>
+            <p>Venkaiah Naidu Affirms Women Empowerment 'essential For Accelerated National Progress'</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={img5}
+            alt="Third slide"
+            style={{ maxHeight: "400px" }}
+          />
+          <Carousel.Caption>
+            <h5>Third slide label</h5>
+            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    </div>  </center>
+
   )
 }
 export default Caro;
--- src/components/Registration_Hooks.js
+++ src/components/Registration_Hooks.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { Link } from 'react-router-dom';
 import axios from 'axios';
 import Navbars from './Navbars';
 import Footer1 from './Footer1';
